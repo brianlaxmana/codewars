96efd910cc50f4503b3cc6e9b8ecbe3f414fb8e4
Brian Anggi Laxmana Putra <bputra@indosoft.dc>
2018-09-24 10:19:13 +0700

    first commit

---
+++ button.js
@@ -0,0 +1,10 @@
+function btn_input()
+{
+  var a=document.getElementById("div_input");
+  a.innerHTML+="<p> lorem ipsum dolor si amet....<p>";
+}
+function btn_output()
+{
+  var a=document.getElementById("div_output");
+  a.innerHTML+="<p> dolor si amet. .. . . <p>";
+}
