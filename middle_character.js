add219341bbbcddb7841628d50915f680d898594
Brian Anggi Laxmana Putra <bputra@indosoft.dc>
2018-09-24 11:03:20 +0700

    progress 1

---
+++ middle_character.js
@@ -0,0 +1,10 @@
+function getMiddle(s)
+{   
+    var middle = "iki rencananya tk pakek gae sting nya"
+    var getmiddle = middle.slice ((middle.length)/2); // iki function seng ate tk pakek
+
+    //var a=document.getElementById("mid-char-output");
+    //a.innerHTML+="<p> dolor si amet. .. . . <p>";
+ 
+  // return s.slice ((s.length)/2); // iki functionnya dari compile online nya
+}
